e576f5b464ed161d8e976baa1d7fb371d63b1731
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-11-16 19:09:13 -0800

    start components, need to think more on pages

---
+++ reactportfolio/src/pages/AboutMe.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+function AboutMe() {
+    return(
+{/* <p>
+My name is Daniel Cuadra. I am a US Army Intelligence Corps veteran. I worked in the intelligence community for 8 years under various commands and roles.
+                After I completed my service contract with the army, I went back to school. First, I went to community college, then to San Francisco State University.
+                I majored in International Relations (diplomacy studies) with an emphasis in economy. After I completed my bachelors degree, I began working at a startup
+                company called Vagaro. I started working as technical support, as I had a technical background before my military service. I had studied C++ and worked
+                toward a CCNA. After working at Vagaro for 5 years, I was assigned numerous roles, to include: Technical Support Management, Product Management, Merchant
+                Services, Sales, and worked on an AI bot. After completing the AI bot project, I rediscovered my passion for coding.
+</p> */}
+    )
+}
