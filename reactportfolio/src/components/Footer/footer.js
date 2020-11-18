b802b4b0a433ac971b85b095c10d17790bf730d6
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-11-18 09:42:41 -0800

    project component add, need to test

--- reactportfolio/src/components/Footer/footer.js
+++ reactportfolio/src/components/Footer/footer.js
@@ -7,8 +7,11 @@ function Footer() {
         <footer className="footer">
             <div className="info-links">
             <a href="https://www.linkedin.com/in/daniel-cuadra-722968183/" target="_blank">
-            <img src={linkedin}></img>
+            <img src={linkedin} alt="linkedin"></img>
             </a>
+            {/* <a href="">
+                <img src={}></img>
+            </a> */}
             </div>
             <span> Copyright © Daniel Cuadra</span>
         </footer>
--- reactportfolio/src/components/ProjectSection/projects.js
+++ reactportfolio/src/components/ProjectSection/projects.js
@@ -4,7 +4,21 @@ import "./style.css";
 function Projects(props) {
     return (
         <div className="project-card">
-            
+           <a href={props.link}>
+               <div className="card">
+                   <img src={props.image} 
+                   style={{width:"200px", height:"200px"}}
+                   className="card-img-top project-icons"
+                   alt={props.title}
+                   target="_blank" />
+                   <div className="card-body">
+                       <h3 className="card-title">{props.title}</h3>
+                       <p className="card-text">{props.description}</p>
+                   </div>
+               </div>
+           </a>
         </div>
     )
 }
+
+export default Projects
