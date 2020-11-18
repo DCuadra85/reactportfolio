3bf4c94fe2e42d7e5985dec72260eceb9cd747da
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-11-18 09:27:18 -0800

    navbar working

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar/navbar"
 import Footer from "./components/Footer/footer"
+// import Portfolio from "./pages/portfoliopage"
 import AboutMe from "./pages/AboutMe"
 
 function App() {
   return (
-
+<>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -21,11 +24,22 @@ function App() {
           Learn React
         </a>
       </header>
-      <AboutMe />
+      <Router>
+      <Navbar />
+    <Switch>
+      <Route exact path={["/", "/aboutme"]}>
+        <AboutMe />
+      </Route>
+      {/* <Route exact path="/portfolio">
+      <Portfolio />
+      </Route> */}
+    </Switch>
+     
       <Footer />
+      </Router>
     </div>
 
-
+</>
   );
 }
 
